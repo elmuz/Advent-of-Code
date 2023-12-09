2157c4c101a30d7e2275ce305bbce7ffe4f96869
elmuz <alessio_elmi@hotmail.com>
2023-12-09 13:56:23 +0100

    Adding day 9

---
+++ 9/aoc-9-1.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import List
+
+
+def compute_differences(row: List[int]) -> List[int]:
+    output = []
+    for a, b in zip(row[:-1], row[1:]):
+        output.append(b - a)
+    return output
+
+def extrapolate_one_line(line: str) -> int:
+    row = [int(n) for n in line.split(" ")]
+    rows = [row]
+    while sum(row) != 0:
+        row = compute_differences(row)
+        rows.append(row)
+    i = 0
+    for row in rows[::-1]:
+        i += row[-1]
+    return i
+
+
+
+def run(lines: List[str]) -> int:
+    extrapolations = [extrapolate_one_line(line.strip()) for line in lines]
+    return sum(extrapolations)
+
+if __name__ == '__main__':
+    assert run(["0 3 6 9 12 15", "1 3 6 10 15 21", "10 13 16 21 30 45"]) == 114
+    with Path("aoc-9.txt").open() as f:
+        lines = f.readlines()
+    print(run(lines))
